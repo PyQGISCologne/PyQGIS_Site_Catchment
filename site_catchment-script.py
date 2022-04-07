089b4ecc52441801b21ae90dc49326520b2d7947
Sara <sara.schiesberg@planumnull.de>
2022-04-07 17:45:01 +0200

    gesäuberte Version, Kommentare auf Englisch

--- site_catchment-script.py
+++ site_catchment-script.py
@@ -1,42 +1,34 @@
-#******************************************************************************************************************************************************
-# Puffer; Flächenanteile pro Punkt # 
-#***********************************************************************************************************************************
+# Title: Site Catchment Analysis - A scripted PyQGIS solution
+# Authors: Sophie Schmidt, Sara Schiesberg, Kai Vogel, Sandra Rung
+# Objective: Takes a point and a polygon layer. Returns a table giving probabilities for polygon types around each point.  
 
+# Import libraries 
 from qgis.PyQt.QtCore import QVariant
 from qgis.core import QgsVectorFileWriter
-import pandas as pd
-import numpy as np
 
-# Dateneingabe: Pfade
-samp = "INSERT_YOUR_ABSOLUT_PATH_TO_SITES"
+# Enter paths to your data
 land = "INSERT_YOUR_ABSOLUT_PATH_TO_POLYGON_LAYER"
+samp = "INSERT_YOUR_ABSOLUT_PATH_TO_POLYGON_LAYER"
 
-# Als Karte laden, addVectorLayer() gibt einen QgsVectorLayer aus mit dem man arbeiten kann
-samples = iface.addVectorLayer(samp, "sample", "ogr")
+# Add the new vector layers to QGIS
 landuse = iface.addVectorLayer(land, "landuse", "ogr")
+samples = iface.addVectorLayer(samp, "sample", "ogr")
 
-# Ein neuer Vektorlayer wird erstellt (ergebnis)
+# Create a new layer for results
 result = QgsVectorLayer("Point?crs=EPSG:32628", "Result", "memory")
 prov = result.dataProvider()
-
-# Diesem Layer werden variablen hinzugefügt
-prov.addAttributes([QgsField("id", QVariant.Int), QgsField("obj", QVariant.String), 
-QgsField("area", QVariant.Double), QgsField("area%", QVariant.Double)])
-result.updateFields() # tell the vector layer to fetch changes from the provider
+prov.addAttributes([QgsField("id", QVariant.Int), QgsField("obj", QVariant.String), QgsField("area", QVariant.Double), QgsField("area%", QVariant.Double)])
+result.updateFields()
 fields = prov.fields()
 
-# Eine leere Liste wird erstellt
+# Nested loop iterating for every point over all polygons
 feats = []
-boden = landuse.getFeatures()
-
-# Die Schleife erzeugt für jeden Punkt alle Zeilen aus der Bodentabelle
 for sample in samples.getFeatures():
     for poly in landuse.getFeatures():
-        # 100 units buffer with a 25 segments boundary, es wird ein obj. "qgis._core.QgsGeometry" erzeugt
-        puffer = sample.geometry().buffer(100, 25)
+        puffer = sample.geometry().buffer(100, 25) # buffer with 100 units and a 25 segments boundary
         part = puffer.intersection(poly.geometry())
-        feat = QgsFeature(fields) # es wird ein objekt "qgis._core.QgsFeature" erzeugt 
-        feat.setGeometry(sample.geometry()) # den features werden die geometrien der samples zugewiesen
+        feat = QgsFeature(fields) 
+        feat.setGeometry(sample.geometry())
         feat['id'] = sample["ID_AR_SITE"]
         feat ["obj"] = poly["Landtype"]
         feat['area'] = part.area()
@@ -44,25 +36,11 @@ for sample in samples.getFeatures():
             feat['area%'] = (part.area())/(puffer.area())
             feats.append(feat)
 
-prov.addFeatures(feats) # hier wird dem provider des result-layers die daten zugewiesen
+# Add the feat
+prov.addFeatures(feats)
 QgsProject.instance().addMapLayer(result)
 
-# Alle leeren Zeilen löschen
-#caps = result.dataProvider().capabilities()
-bod = result.getFeatures()
-#dfeats = []
-
-#for feat in bod: 
-#   if feat ["area"]==0:
-#        dfeats.append(feat.id())
-
-#res = result.dataProvider().deleteFeatures(dfeats)
-# QgsProject.instance().addMapLayer(result)
-
-# Den Datensatz als csv exportieren
-QgsVectorFileWriter.writeAsVectorFormat(result, "YOUR_ABSOLUT_PATH_TO_ESULT_CSV","UTF-8", 
+# Save the results as .csv
+QgsVectorFileWriter.writeAsVectorFormat(result, "INSERT_YOUR_ABSOLUT_PATH_TO_POLYGON_LAYER","UTF-8", 
 result.crs(), "CSV")
 
-# Pandas
-tabelle = pd.read_csv("YOUR_ABSOLUT_PATH_TO_ESULT_CSV")
-kreuztabelle = pd.pivot_table (tabelle, values='area%', index=['id'],columns=['obj'], aggfunc=np.sum)
